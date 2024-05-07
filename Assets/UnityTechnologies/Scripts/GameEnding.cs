3de6ce0b40c08102e5dbf7dfe842ddd450701e65
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-05-07 10:51:25 +0100

    Audio
    Adding ambient sound, enemies sounds, footsteps

--- Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -8,6 +8,9 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
 
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+
     public GameObject player;
 
     public CanvasGroup exitBackgroundImageCanvasGroup;
@@ -17,6 +20,7 @@ public class GameEnding : MonoBehaviour
     bool m_IsPlayerCaught;
 
     float m_Timer;
+    bool m_HasAudioPlayed;
 
     void OnTriggerEnter(Collider other)
     {
@@ -34,15 +38,21 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
